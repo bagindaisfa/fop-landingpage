d2c95a2594582dcf42242efc0dde917aada4a515
bagindaisfa <bagindaisfa05@gmail.com>
2025-08-27 17:20:38 +0700

    first commit

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,12 @@
+
+
+const MainContent = () => {
+  return (
+    <div className="main-content">
+      <h1>Welcome to Field Of Paradise</h1>
+      <p>Your main content goes here.</p>
+    </div>
+  );
+};
+
+export default MainContent;
