134080667e9de628ab239e3721746997378b82bc
bagindaisfa <bagindaisfa05@gmail.com>
2025-09-11 21:38:26 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ export default function Navbar() {
             <img
               src={logoImg}
               alt="Field of Paradise Logo"
-              className="h-10 w-auto md:h-12"
+              className="h-12 w-auto md:h-16 lg:h-20 transition-all duration-300"
             />
           </div>
 
